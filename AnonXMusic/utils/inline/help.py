1bb57b3ff57ee114f7ce99ac5de7b2c10d1a8d4b
乛𝒙𝑫 ۵ [🇮🇳] <130631732+UltraProMaxNooB@users.noreply.github.com>
2024-07-03 10:09:07 +0530

    Update help.py
    Signed-off-by: 乛𝒙𝑫 ۵ [🇮🇳] <130631732+UltraProMaxNooB@users.noreply.github.com>

--- AnonXMusic/utils/inline/help.py
+++ AnonXMusic/utils/inline/help.py
@@ -89,6 +89,7 @@ def help_pannel(_, START: Union[bool, int] = None):
                 InlineKeyboardButton(
                     text=_["H_B_20"],
                     callback_data="help_callback hb20",
+        
             ],
         mark,
         ]
