a5fa1c19a8452be85a3b546f89387e3697be0f1a
Dev meena <btwdevxd@gmail.com>
2024-07-14 22:28:53 +0530

    Update start.py
    Signed-off-by: Dev meena <btwdevxd@gmail.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -26,7 +26,7 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/AIPHA_SUPPORT"),
+            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/T0XIC_SQUAD"),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
         [
