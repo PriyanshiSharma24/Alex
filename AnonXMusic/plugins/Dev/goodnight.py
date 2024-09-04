78da332644e8bf1f4ebecdfc56e1c250f52b64d9
Dev meena <btwdevxd@gmail.com>
2024-09-04 14:13:21 +0530

    Update goodnight.py
    Signed-off-by: Dev meena <btwdevxd@gmail.com>

--- AnonXMusic/plugins/Dev/goodnight.py
+++ AnonXMusic/plugins/Dev/goodnight.py
@@ -9,7 +9,7 @@ from AnonXMusic import app
 
 
 # "/gn" command ka handler
-@app.on_message(filters.command("goodnight", prefixes="g"))
+@app.on_message(filters.command("gn", prefixes="g"))
 def goodnight_command_handler(client: Client, message: Message):
     # Randomly decide whether to send a sticker or an emoji
     send_sticker = random.choice([True])
